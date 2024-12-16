d13ca56f974270d342f40217fed0abdbdb5332cf
Jhonny Vicmar Claure Vedia <claurejhonnny188@gmail.com>
2024-12-16 16:55:10 +0100

    update

--- c/ejercicios/actividades/act04/act01.c
+++ c/ejercicios/actividades/act04/act01.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+//Estructura de los libros
 struct Book {
   char title[100];
   char autor[100];
@@ -8,6 +9,7 @@ struct Book {
   char isbn[10];
 };
 
+// Funcion para el menu de opciones (actualemnte 4 opciones).
 void menuInicial() {
   printf("Personal Library\n");
   printf("Menu: \n");
@@ -17,22 +19,23 @@ void menuInicial() {
   printf("4. Exit\n");
 }
 
+//Funcion para obtener la opcion del menu del usuario devuelta a trave de un puntero.
 void obtainOption(int *iOption){
   printf("Escoger opcion: ");
   scanf("%d", iOption);
 }
 
+//Funcion para obtener la infromacion (title, autor, publication_date, isbn) de libro añadido a biblioteca.txt.
 void obtainDataUser(struct Book *book){
   printf("Insert data: \n");
-  
   printf("> Title: ");
   getchar(); //Eliminar del buffer el espacio para que fhgets() no lo lea
   fgets(book->title, 100, stdin); //Para obtnere linea de caracteres con espacio.
-  strtok(book->title, "\n"); // Eliminar de la variable title el salto de linea
+  strtok(book->title, "\n"); //Eliminar de la variable title el salto de linea añadido por fgets()
 
   printf("> Autor: ");
   fgets(book->autor, sizeof(book->autor), stdin);
-  strtok(book->autor, "\n"); // Eliminar de la variable title el salto de linea
+  strtok(book->autor, "\n"); //Eliminar de la variable book el salto de linea añadido por fgets()
 
   printf("> Publication Date: ");
   scanf("%d", &book->publicationDate);
@@ -41,22 +44,26 @@ void obtainDataUser(struct Book *book){
   scanf("%s", book->isbn);
 }
 
+//Funcion para obtener el ISBN de busqueda del usuari.
 void obtainIsbnUser (char cIsbn[10]){
   printf("Enter the ISBN of the book: ");
   scanf("%s", cIsbn);
 }
 
+//Funcion reutilizable para leer el archivo biblioteca.txt, la lectura es em modo "r", si no se detecta el archvio duevelve NULL.
+//Es una funcion que retorna un dato tipo FILE por eso ponemo FILE en vez de void().
 FILE *readFile(char filePath[]){
   FILE *file;
-  file = fopen(filePath, "r");
+  file = fopen(filePath, "r"); //Abrimo el arxivo pasado por el parametro en modo lectura "r".
   
   if (file == NULL){
     return NULL;
   }
 
-  return file;
+  return file; // Devolvemos el arxivo obtenido.
 }
 
+//Funcion reutilizable para escribir en el archivo biblioteca.txt, la escritura es en modo "a+", si no se detecta el archvio duevelve NULL.
 FILE *writeFile(char filePath[]){
   FILE *file;
   file = fopen(filePath, "a+");
@@ -67,37 +74,55 @@ FILE *writeFile(char filePath[]){
   return file;
 }
 
+//Funcion para añadir el libro que obtinen por paramteros el path del arxivo y la estructura del libro con los datos insertados del usuario.
 void addBook(char filePath[], struct Book *book){
   FILE *file = writeFile(filePath);
+  //Si el arxivo nos retorna nulo cerramos el arxivo i mostramos un mensaje de error.
   if (file == NULL){
     printf("Error: not add book because...\n");
+    fclose(file);
     return;
   }
+
+  //Insertamos los datos en la estructura Book en el arxivo, con formato CSV (title, autor, book, isbn).
   fprintf(file, "%s, %s, %d, %s", book->title, book->autor, book -> publicationDate, book->isbn);
-  fclose(file);
+  fclose(file); //Cerramos el arxivo una vez insertado los datos.
 }
 
+//Funcion para listar todos lo libros en el documento pasado como argumento "biblioteca.txt".
 void listBooks(char filePath[]) {
-  FILE *file = readFile(filePath);
+  FILE *file = readFile(filePath); //Obtenemo el arxivo o NUll a traves de la funcion readFile()
   
+  //Si en file obtenemos null mostramos un mensaje de error, cerramos el arrxivo y paramos la funcion con return. 
   if (file == NULL){
     printf("Error: not list books...\n");
+    fclose(file);
     return;
   }
 
-  char linea[100]; 
+  //Creamos una variable llamada cLineDocument encargada de almacenar la linea revisada con fgets().
+  char cLineDocument[100]; 
   printf("---\n");
-  while (fgets(linea, sizeof(linea), file) != NULL) {
-    printf("%s", linea);
+  
+  //Comenzamos un cicilo while que como comprovante ejecuta el metdod fgets() en el documento encargado de recorer las lineas del documento. 
+  //Si en la linea que esta revisando no es NULL imprime la linea almacenada en cLineDOcument. En canvio si es NULL para el ciclo y no 
+  //imprime mas lineas. Hay que tener en cuenta que fgets() añade uan "\n" al final de cada fila que no ahorra de añadir en el printf().
+  //En fgets le pasamo como primer arguneto cLineDOcument para indicar donde se guardara, como segundo argumento le pasamo el tamaño de 
+  //cLineDocuments con el metodo sizeof() y como tercer y ultimo argumento de que arxivo obtienen la infromacion (en este se teinen que tener 
+  //en quenta que el cursosr se queda en la possicion inmediata despues de la linea leida).
+
+  while (fgets(cLineDocument, sizeof(cLineDocument), file) != NULL) {
+    printf("%s", cLineDocument);
   }
   printf("\n---\n");
-  fclose(file);
+  fclose(file); //Cerramos el arxivo
 }
 
 void searchBook(char filePath[], char cUserISBN[]){
   FILE *file = readFile(filePath);
   if (file == NULL){
     printf("Error: not search books...\n");
+    fclose(file);
     return;
   }
 
@@ -113,24 +138,27 @@ void searchBook(char filePath[], char cUserISBN[]){
       break;
     }
     // Obtenemos el tamaño de la oracion(.txt) [1...100] y le restamos 1 para ir caracter en caracter en la array [0...99]
-    iContextLength = strlen(context) - 1;    
+    iContextLength = strlen(context);
+
+    if (context[iContextLength - 1] == '\n') {
+      context[iContextLength - 1] = '\0';
+      iContextLength--; // Actualizamos la longitud
+    }
 
     for (int i = 1; i < 10; i++){
       sISBN[9 - i] = context[iContextLength - i];  
     }
+    //Añadimos \0 para que la palabra tenga final
     sISBN[9] = '\0';
   
+    printf("%s-%s\n", sISBN, cUserISBN);
     if (strcmp(sISBN, cUserISBN) == 0){
       //printf("The ISBN is equal!\n");
-      //printf("%s-%s\n", sISBN, cUserISBN);
       printf("%s\n", context);
       iVoucher = 0;
-    } else{
-      printf("This book is doesn't exits in local library!.\n\n");
-      iVoucher = 0;
     }
-
   } while(iVoucher == 1);
+  fclose(file);
 }
 
 int main() {
